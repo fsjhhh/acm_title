b0ce9f9db555f37d6cf572941de3460b31dc9707
fsj <1741901362@qq.com>
2024-02-18 18:17:35 +0800

    change .gitignore

---
+++ cpp/codeforces/925/A.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <deque>
+#include <iomanip> // std::fixed std::setprecision(x) 保留x位小数
+#include <array>
+#include <bitset>
+#include <functional>
+#include <ranges>
+// #include <bits/stdc++.h>
+// priority_queue 优先队列
+// std::cout.flush(); 交互题
+#define IOS                           \
+    std::ios::sync_with_stdio(false); \
+    std::cin.tie(0);                  \
+    std::cout.tie(0);
+
+typedef long long LL;
+typedef __int128 i128;
+typedef std::pair<int, int> PII;
+typedef std::pair<LL, LL> PLL;
+const int INF = 0x3f3f3f3f;
+const LL INFL = 0x3f3f3f3f3f3f3f3f;
+
+void solve() {
+    int n;
+    std::cin >> n;
+    for (int i = 0; i < 26; i++) {
+        for (int j = 0; j < 26; j++) {
+            for (int k = 0; k < 26; k++) {
+                if (i + j + k + 3 == n) {
+                    std::cout << (char)(i + 'a') << (char)(j + 'a') << (char)(k + 'a') << "\n";
+                    return ;
+                }
+            }
+        }
+    }
+}
+
+int main() {
+    IOS;
+    int t = 1;
+    std::cin >> t;
+    while (t -- )
+        solve();
+    return 0;
+}
